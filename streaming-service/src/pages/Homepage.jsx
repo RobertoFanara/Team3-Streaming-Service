af1f319e1148ad68fa48e520c42702df6d327dd9
RobertoFanara <fanararoberto7@gmail.com>
2023-06-07 11:13:39 +0200

    yes

--- streaming-service/src/pages/Homepage.jsx
+++ streaming-service/src/pages/Homepage.jsx
@@ -2,18 +2,14 @@ import Navbar from "../components/Navbar";
 function Homepage() {
   return (
     <>
-      <div className="bg-black w-full h-[100vh]">
+      <div className="bg-black w-full h-[100vw]">
         <div className="flex flex-col justify-center items-center gap-5">
           <Navbar />
 
-          <div className="border border-2 border-[RGB(255,187,56)] rounded-[20px] w-[870px] h-[450px] m-5 ">
-            <img />
-            <p>The Hangover</p>
-            <div className="flex justify-center items-center gap-5 w-[280px] h-[60px]">
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
+          <div className="flex border border-2 border-[RGB(255,187,56)] rounded-[20px] w-[870px] h-[450px] m-5 "></div>
+          
+          <div className="flex flex-col w-full ">
+            <p>Trending</p>
           </div>
         </div>
       </div>
