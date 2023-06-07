f5772505aed5bb200333c6dfe0d3b36ff6910337
RobertoFanara <fanararoberto7@gmail.com>
2023-06-07 10:47:38 +0200

    created border main film on homepage

--- streaming-service/src/pages/Homepage.jsx
+++ streaming-service/src/pages/Homepage.jsx
@@ -7,11 +7,13 @@ function Homepage() {
           <Navbar />
 
           <div className="border border-2 border-[RGB(255,187,56)] rounded-[20px] w-[870px] h-[450px] m-5 ">
-            <img/>
-            <p></p>
-            <div></div>
-            <div></div>
-            <div></div>
+            <img />
+            <p>The Hangover</p>
+            <div className="flex justify-center items-center gap-5 w-[280px] h-[60px]">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
           </div>
         </div>
       </div>
