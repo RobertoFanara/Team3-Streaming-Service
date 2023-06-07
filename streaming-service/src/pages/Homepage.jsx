611bd35a9fa20bd4607a91cadc253bb3fa7ca8eb
RobertoFanara <fanararoberto7@gmail.com>
2023-06-07 10:27:08 +0200

    added heroicons

--- streaming-service/src/pages/Homepage.jsx
+++ streaming-service/src/pages/Homepage.jsx
@@ -2,11 +2,17 @@ import Navbar from "../components/Navbar";
 function Homepage() {
   return (
     <>
-      <div className="flex flex-col justify-center align-center bg-black">
-        <Navbar />
+      <div className="bg-black w-full h-[100vh]">
+        <div className="flex flex-col justify-center items-center gap-5">
+          <Navbar />
 
-        <div className="flex border-2 w-10 h-10">
-
+          <div className="border border-2 border-[RGB(255,187,56)] rounded-[20px] w-[870px] h-[450px] m-5 ">
+            <img/>
+            <p></p>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
       </div>
     </>
