18f269c9efb5aba2d56b9fe37ee3aff5c598952f
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-06-15 18:50:14 +0200

    fixing thing

--- streaming-service/src/components/OurRaccomandation.jsx
+++ streaming-service/src/components/OurRaccomandation.jsx
@@ -4,7 +4,7 @@ const OurRaccomandation = () => {
         <>
             <div className="w-full h-[450px] mt-10 flex flex-col align-center justify-center">
                 <div className="text-white">Our Raccomandation</div>
-                <div className="w-full h-full relative mt-2 bg-[url('https://static.sky.it/images_static/tg24/spettacolo/2019/08/14/James_Cameron/James_Cameron_Avatar.jpg.transform/hero-tablet/ffd5e21ae3ab3803fee4805ee5532d1bc3304f7a/img.jpeg')] bg-center bg-cover bg-no-repeat border-2 rounded-lg border-yellow-400">
+                <div className="w-full h-full relative mt-2 bg-[url('https://static.sky.it/images_static/tg24/spettacolo/2019/08/14/James_Cameron/James_Cameron_Avatar.jpg.transform/hero-tablet/ffd5e21ae3ab3803fee4805ee5532d1bc3304f7a/img.jpeg')] bg-center bg-cover bg-no-repeat border-2 rounded-lg border-[RGB(255,187,56)]">
                     <div className="flex absolute w-[80px] h-[75px] bottom-[30px] left-[25px]  hover:fill-yellow-400 hover:text-yellow-400">
                         <svg className="absolute stroke-[rgb(255,187,65)] fill-[rgb(255,187,65)] " xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" id="star"><path d="M22,10.1c0.1-0.5-0.3-1.1-0.8-1.1l-5.7-0.8L12.9,3c-0.1-0.2-0.2-0.3-0.4-0.4C12,2.3,11.4,2.5,11.1,3L8.6,8.2L2.9,9
 	                    C2.6,9,2.4,9.1,2.3,9.3c-0.4,0.4-0.4,1,0,1.4l4.1,4l-1,5.7c0,0.2,0,0.4,0.1,0.6c0.3,0.5,0.9,0.7,1.4,0.4l5.1-2.7l5.1,2.7
--- streaming-service/src/pages/Categories.jsx
+++ streaming-service/src/pages/Categories.jsx
@@ -2,6 +2,7 @@ import OurRaccomandation from "../components/OurRaccomandation"
 import Others from "../components/Others"
 import Navbar from "../components/Navbar"
 import Sidebar from "../components/Sidebar"
+import Footer from "../components/Footer"
 
 const Categories = () => {
     return(
@@ -9,11 +10,12 @@ const Categories = () => {
         <Navbar />
         <Sidebar />
         <div className="flex item-center justify-center ">
-            <div className="border border-black h-screen w-[870px]">
+            <div className="flex flex-col border border-black w-[870px] gap-8">
                 <OurRaccomandation />
                 <Others />
             </div>
         </div>
+        <Footer />
         </>
     )
 }
