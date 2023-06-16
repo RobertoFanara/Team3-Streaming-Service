dac797fe6503a1ceb468ba38ee19ce60e979d3a6
RobertoFanara <fanararoberto7@gmail.com>
2023-06-16 22:57:38 +0200

    secondo merge

--- streaming-service/src/pages/Categories.jsx
+++ streaming-service/src/pages/Categories.jsx
@@ -1,21 +1,16 @@
 import OurRaccomandation from "../components/OurRaccomandation"
 import Others from "../components/Others"
-import Navbar from "../components/Navbar"
-import Sidebar from "../components/Sidebar"
-import Footer from "../components/Footer"
+
 
 const Categories = () => {
     return(
         <>
-        <Navbar />
-        <Sidebar />
         <div className="flex item-center justify-center ">
             <div className="flex flex-col border border-black w-[870px] gap-8">
                 <OurRaccomandation />
                 <Others />               
             </div>
         </div>
-        <Footer />
         </>
     )
 }
