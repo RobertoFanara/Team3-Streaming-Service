6fdd267a1a815492450ad993b632e12873959b53
gabrimarch3 <gabrimarche@protonmail.com>
2023-06-21 22:04:24 +0200

    fixed one query

--- streaming-service/src/components/Recommendation.jsx
+++ streaming-service/src/components/Recommendation.jsx
@@ -1,17 +1,37 @@
 import React from "react";
 import Slider from "react-slick";
+import { useState, useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Recommendation.css";
 
 const Recommendations = ({ recommendations }) => {
-  const settings = {
+  const [settings, setSettings] = useState({
     dots: true,
     infinite: true,
     speed: 100,
     slidesToShow: 2,
     slidesToScroll: 1,
-  };
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 2000) {
+        setSettings((prevSettings) => ({
+          ...prevSettings,
+          slidesToShow: 1,
+        }));
+      } else {
+        setSettings((prevSettings) => ({
+          ...prevSettings,
+          slidesToShow: 2,
+        }));
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div className="recommendations">
