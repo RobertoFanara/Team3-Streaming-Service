15f0ee492a6aa846d363a51135c959d9cf5a49b6
Alfio Puglisi <puglisi_algio@ciao.it>
2023-06-21 21:59:24 +0200

    dropdown profile

--- streaming-service/src/components/Footer.jsx
+++ streaming-service/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import twitter from "../assets/images/twitter.svg"
 
 function Footer() {
   return (
-    <footer className="bg-[#ffbb38] p-6 m-5">
+    <footer className="bg-[#ffbb38] p-6 mt-5">
       <div className="container mx-auto">
         <div className="flex flex-wrap justify-between">
           <div className="footer_logo">
