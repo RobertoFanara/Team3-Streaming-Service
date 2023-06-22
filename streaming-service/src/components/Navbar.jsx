adf8651959c16758648b8ba868118ffae4ea7bd0
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-06-22 11:34:36 +0200

    fixed things

--- streaming-service/src/components/Navbar.jsx
+++ streaming-service/src/components/Navbar.jsx
@@ -90,6 +90,22 @@ const handleCategoriesDisappear = () => {
   )
 }
 
+  const handleFocusCategory = (event) => {
+    useEffect(() =>{
+      const $category1 = document.querySelector(".category1");
+      const $category2 = document.querySelector(".category2");
+      const $category3 = document.querySelector(".category3");
+      const $category4 = document.querySelector(".category4");
+      const $category5 = document.querySelector(".category5");
+      const $category6 = document.querySelector(".category6");
+      const handleFocusCategory1 = () => {
+        $category1.addClass("text-white")
+      }
+      const category1Function = handleFocusCategory1;
+      
+    })
+  }
+
 
   return (
     <>
@@ -120,9 +136,9 @@ const handleCategoriesDisappear = () => {
                 </p>
               </Link>
             </div>
-                <div className="relative category-list hidden h-[100%]">
+                <div className="relative category-list hidden h-[100%] w-[300px] bg-[rgb(255,187,56)] rounded-[3px] items-center justify-center">
 
-                  <div className="before-arrow-search absolute bg-trasparent w-[30px] h-full flex justify-center items-center z-50" onClick={handleScrollSearchRight()} >
+                  <div className="before-arrow-search absolute start-0 bg-trasparent w-[30px] h-full flex justify-center items-center z-50" onClick={handleScrollSearchRight()} >
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
                       </svg>
@@ -135,13 +151,13 @@ const handleCategoriesDisappear = () => {
                     </div>
 
 
-                  <div className="slider-search h[100%] flex w-[300px] gap-[1rem] overflow-hidden px-[28px] bg-[rgb(255,187,56)] rounded-[3px]" >
-                  <Link to={"/others"} onClick={topFunction} className="py-[10px] flex items-center hover:text-white transition duration-200 ease-in-out"><div >Altro</div></Link>
-                  <Link to={"/azione"} onClick={topFunction}  className="py-[10px] flex items-center hover:text-white transition duration-200 ease-in-out"><div >Azione</div></Link>
-                  <Link to={"/avventura"} onClick={topFunction}  className="py-[10px] flex items-center hover:text-white transition duration-200 ease-in-out"><div >Avventura</div></Link>
-                  <Link to={"/animazione"} onClick={topFunction}  className="py-[10px] flex items-center hover:text-white transition duration-200 ease-in-out"><div >Animazione</div></Link>
-                  <Link to={"/comico"} onClick={topFunction}  className="py-[10px] flex items-center hover:text-white transition duration-200 ease-in-out"><div >Comico</div></Link>
-                  <Link to={"/drammatico"} onClick={topFunction}  className="py-[10px] flex items-center hover:text-white transition duration-200 ease-in-out"><div >Drammatico</div></Link>
+                  <div className="slider-search h[100%] flex w-[240px] gap-[1rem] overflow-hidden" >
+                  <Link to={"/others"} onClick={topFunction} className="py-[10px] flex items-center hover:text-white transition duration-200 ease-in-out"><div className="category1" onClick={handleFocusCategory(() => category1Function)}>Altro</div></Link>
+                  <Link to={"/azione"} onClick={topFunction}  className="py-[10px] flex items-center hover:text-white transition duration-200 ease-in-out"><div className="category2" >Azione</div></Link>
+                  <Link to={"/avventura"} onClick={topFunction}  className="py-[10px] flex items-center hover:text-white transition duration-200 ease-in-out"><div className="category3" >Avventura</div></Link>
+                  <Link to={"/animazione"} onClick={topFunction}  className="py-[10px] flex items-center hover:text-white transition duration-200 ease-in-out"><div className="category4" >Animazione</div></Link>
+                  <Link to={"/comico"} onClick={topFunction}  className="py-[10px] flex items-center hover:text-white transition duration-200 ease-in-out"><div className="category5" >Comico</div></Link>
+                  <Link to={"/drammatico"} onClick={topFunction}  className="py-[10px] flex items-center hover:text-white transition duration-200 ease-in-out"><div className="category6" >Drammatico</div></Link>
                   </div>
                 </div>
           </div>
